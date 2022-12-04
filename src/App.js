60a45ad3aff0c7a35fe55ac503f48a8058a7edb3
devWeibel <weibel.jason@gmail.com>
2022-12-04 18:39:05 -0500

    removed tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Time to make a Portfolio for real
         </a>
       </header>
     </div>

4200eab4833e33465eb4e78fc566c6ab3cfb9c85
devWeibel <weibel.jason@gmail.com>
2022-12-07 12:54:58 -0500

    Navbar and Projects work

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+    return (
+        <div className='navContainer'>
+            <Link to='/jason-weibel-portfolio'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/projects'>Projects</Link>
+            <Link to='/resume'>Resume</Link>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Projects.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react"
+import RadialMenu from "react-radial-menu"
+
+const items = [
+	{"href":"http://www.facebook.com", "image":"url(examples/dist/images/social/facebook.png)"},
+	{"href":"http://www.reddit.com", "image":"url(examples/dist/images/social/reddit.png)"},
+	{"href":"http://www.flickr.com", "image":"url(examples/dist/images/social/flickr.png)"},
+	{"href":"http://www.google.com", "image":"url(examples/dist/images/social/googleplus.png)"},
+	{"href":"http://www.linkedin.com", "image":"url(examples/dist/images/social/linkedin.png)"},
+	{"href":"http://www.twitter.com", "image":"url(examples/dist/images/social/twitter.png)"},
+	{"href":"http://www.twitter.com", "image":"url(examples/dist/images/social/twitter.png)"}
+];
+
+const center = {
+	"image": "url(examples/dist/images/social/share.png)"
+};
+
+export default class App extends Component {
+    render() {
+        const {items, center} = this.props
+        return <RadialMenu
+          items={items}
+          center={center}
+        />
+    }
+}
+
+module.exports = App;
+
+React.render(<App/>, document.body);

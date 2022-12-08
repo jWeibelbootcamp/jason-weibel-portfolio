5e2e6da97885d49e91fcadb56c3e3ffb3f6de181
devWeibel <weibel.jason@gmail.com>
2022-12-08 08:39:53 -0500

    created component files. added sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
     </div>
   );
 }

4a650cb33554b7288e1b0f4146d122eb83ac4721
devWeibel <weibel.jason@gmail.com>
2022-12-10 08:05:58 -0500

    footer links styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import './Header.scss';
 
 const Header = () => {
     return (
-        <Navbar>
+        <Navbar expand='lg'>
             <Container>
-                <Navbar.Toggle />
-                <Navbar.Collapse>
+                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                <Navbar.Collapse id='basic-navbar-nav'>
                     <Link to='/'>Home</Link>
                     <Link to='/about'>About</Link>
                     <Link to='/projects'>Projects</Link>

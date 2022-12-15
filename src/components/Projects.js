0b23b36ae830d463294b3ad65a9d8f11df2106f3
devWeibel <weibel.jason@gmail.com>
2022-12-15 18:52:30 -0500

    abandoned readial menu for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,38 +1,6 @@
 import React, { Component } from "react";
 import RadialMenu from "react-radial-menu";
+import './Projects.scss';
 
-// center image to click
-const center = {
-    "image": "url(examples/dist/images/social/share.png)"
-};
-
-// expandable images after click
-const items = [
-    { "href": "http://www.facebook.com", "image": "url(examples/dist/images/social/facebook.png)" },
-    { "href": "http://www.reddit.com", "image": "url(examples/dist/images/social/reddit.png)" },
-    { "href": "http://www.flickr.com", "image": "url(examples/dist/images/social/flickr.png)" },
-    { "href": "http://www.google.com", "image": "url(examples/dist/images/social/googleplus.png)" },
-    { "href": "http://www.linkedin.com", "image": "url(examples/dist/images/social/linkedin.png)" },
-    { "href": "http://www.twitter.com", "image": "url(examples/dist/images/social/twitter.png)" },
-    { "href": "http://www.twitter.com", "image": "url(examples/dist/images/social/twitter.png)" }
-];
-
-class Menu extends Component {
-    render() {
-        const { center, items } = this.props
-        return (<RadialMenu
-            center={center}
-            items={items}
-        />);
-    };
-};
-
-const Projects = () => {
-    return (
-        <div>
-            <Menu />
-        </div>
-    );
-};
 
 export default Projects;

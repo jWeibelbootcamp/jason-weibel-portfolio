9e3cd5802df98db2310deb9678d97c6baba0ae02
devWeibel <weibel.jason@gmail.com>
2022-12-16 13:00:45 -0500

    added a tree on home page?  may get rid of

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Anime from "animejs";
+import Tree from "../images/pixelTree.png";
 import './Home.scss';
 
 const Home = () => {
+    let treeAnime = {};
+    treeAnime.opacityIn = [0, 1];
+    treeAnime.scaleIn = [0.2, 1];
+    treeAnime.delay = 500;
+
     let nameAnime = {};
     nameAnime.opacityIn = [0, 1];
     nameAnime.scaleIn = [0.2, 1];
@@ -13,7 +19,7 @@ const Home = () => {
     titleAnime.scaleIn = [0.2, 1];
     titleAnime.delay = 500;
 
-// basic animation in place - update to zoom in from left and right later.
+    // basic animation in place - update to zoom in from left and right later.
 
     const animationRef = React.useRef(null);
 
@@ -29,21 +35,33 @@ const Home = () => {
                 opacity: titleAnime.opacityIn,
                 scale: titleAnime.scaleIn,
                 delay: titleAnime.delay
+            }).add({
+                targets: '.treeAnime .tree',
+                opacity: nameAnime.opacityIn,
+                scale: nameAnime.scaleIn,
+                delay: nameAnime.delay
             })
     });
 
     return (
-        <div className="homeContainer">
-            <div className="homeBody">
-                <h1 className="nameAnime">
-                    <span className="name">Jason Weibel</span>
-                </h1>
+        <div>
+            <div className="homeContainer">
+                <div className="homeBody">
+                    <h1 className="nameAnime">
+                        <span className="name">Jason Weibel</span>
+                    </h1>
+                </div>
+                <div className="homeBody">
+                    <h3 className="titleAnime">
+                        <span className="title">Full Stack Web Developer</span>
+                    </h3>
+                </div>
             </div>
-            <div className="homeBody">
-                <h3 className="titleAnime">
-                    <span className="title">Full Stack Web Developer</span>
-                </h3>
+
+            <div className="treeContainer">
+                <img src={Tree} alt="baren tree" style={{position: 'relative', zIndex: -1}}></img>
             </div>
+        
         </div>
     );
 };
--- src/components/TechCarousel.js
+++ src/components/TechCarousel.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Carousel } from "react-carousel3";
-
-// forced icon size
-const style = {
-    width: 150,
-    height: 175,
-};
+import './TechCarousel.scss';
+import JSLogo from '../images/tech-logos/jslogo.svg';
+import HTMLLogo from '../images/tech-logos/htmllogo.svg';
+import CSSLogo from '../images/tech-logos/csslogo.svg';
+import ExpressLogo from '../images/tech-logos/expresslogo.png';
+import NodeLogo from '../images/tech-logos/nodelogo.svg';
+import MongoLogo from '../images/tech-logos/mongodblogo.svg';
+import MySQLLogo from '../images/tech-logos/mysqllogo.png';
+import ReactLogo from '../images/tech-logos/reactlogo.png';
 
 const TechCarousel = () => (
     <div className="carouselContainer"
@@ -13,24 +16,30 @@ const TechCarousel = () => (
             display: 'flex',
             justifyContent: 'center',
         }}>
-        <Carousel height={200} width={600} xOrigin={340} xRadius={300} yOrigin={80} yRadius={50} autoPlay={true}>
-            <div key={1} style={style}>
-                <img src="/images/tech-logos/javascript.png" alt="javascript" />
+        <Carousel height={200} width={600} xOrigin={300} xRadius={300} yOrigin={80} yRadius={50} autoPlay={true}>
+            <div key={1}>
+                <img className='techImage' src={JSLogo} alt="Javascript logo" />
             </div>
-            <div key={2} style={style}>
-                <img src="/images/tech-logos/react.png" alt="react" />
+            <div key={2}>
+                <img className='techImage' src={HTMLLogo} alt="HTML logo" />
             </div>
-            <div key={3} style={style}>
-                <img src="/images/tech-logos/express.jpg" alt="express" />
+            <div key={3}>
+                <img className='techImage' src={CSSLogo} alt="CSS logo" />
             </div>
-            <div key={4} style={style}>
-                <img src="/images/tech-logos/node.svg" alt="node" />
+            <div key={4}>
+                <img className='techImage' src={ExpressLogo} alt="Express logo" />
             </div>
-            <div key={5} style={style}>
-                <img src="/images/tech-logos/mysql.png" alt="mysql" />
+            <div key={5}>
+                <img className='techImage' src={NodeLogo} alt="NodeJS logo" />
             </div>
-            <div key={6} style={style}>
-                <img src="/images/tech-logos/mongodb.svg" alt="mongodb" />
+            <div key={6}>
+                <img className='techImage' src={MongoLogo} alt="MongoDB logo" />
+            </div>
+            <div key={7}>
+                <img className='techImage' src={MySQLLogo} alt="MySQL logo" />
+            </div>
+            <div key={8}>
+                <img className='techImage' src={ReactLogo} alt="react logo" />
             </div>
         </Carousel>
     </div>

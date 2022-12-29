7ba709b97625fdb13d737fb91aa0ec141313b6eb
devWeibel <weibel.jason@gmail.com>
2022-12-29 16:53:00 -0500

    project screenshots

---
+++ src/components/ProjectData.js
@@ -0,0 +1,69 @@
+
+const projectData = [
+    {
+        title: "Find My Movie",
+        image: findmymovie,
+        description: "Search for movies with information on where to watch online, then save these movies to a list to watch later. API inactive",
+        code: "https://github.com/wendywilgus/FindMyMovie",
+        demo: "https://chrissnakhiev.github.io/BCTW-Team-Project-1/",
+    },
+    {
+        title: "Social Network API",
+        image: socialnetwork,
+        description: "An API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list",
+        code: "https://github.com/wendywilgus/Social-Network-API",
+        demo: "https://drive.google.com/file/d/1wdD6Mxo50sB2QADnGQCLE5QG5e8pZrz4/view",
+    },
+    {
+        title: "E-Commerce Backend",
+        image: ecommerce,
+        description: "An Express.js API that uses Sequelize to interact with a MySQL database",
+        code: "https://github.com/wendywilgus/E-Commerce-Backend",
+        demo: "https://drive.google.com/file/d/1MA6d_44GjfymDqai4HOuBaiN4OMevejk/view",
+    },
+    {
+        title: "Employee Tracker",
+        image: myteam,
+        description: "Content management system (CMS) to view and update a company's employee database. Node.js, Inquirer, and MySQL",
+        code: "https://github.com/wendywilgus/EmployeeTracker",
+        demo: "https://drive.google.com/file/d/1lnAz0c0_hoDQCpU2cX1OXgd_u8M90I6S/view",
+    },
+    {
+        title: "Note Taker App",
+        image: notetaker,
+        description: "Write and save notes using an Express.js back end. Saves and retrieves note data from a JSON file.",
+        code: "https://github.com/wendywilgus/Note-Taker",
+        demo: "https://notesapp1234567.herokuapp.com/",
+    },
+    {
+        title: "Team Profile Generator",
+        image: teamgen,
+        description: "A Node.js command-line app that takes in info about a team, then generates an HTML webpage with summaries for each person",
+        code: "https://github.com/wendywilgus/Team-Profile-Generator",
+        demo: "https://watch.screencastify.com/v/u5101uCoNl6kGYZ3f9tu",
+    },
+
+    {
+        title: "Weather Dashboard",
+        image: weather,
+        description: "A weather app that uses APIs to display the weather outlook for multiple cities over five days",
+        code: "https://github.com/wendywilgus/Weather-Dashboard",
+        demo: "https://wendywilgus.github.io/Weather-Dashboard/",
+    },
+    {
+        title: "Work Day Scheduler",
+        image: scheduler,
+        description: "A calendar app that allows a user to save events for each hour of the day",
+        code: "https://github.com/wendywilgus?tab=repositories",
+        demo: "https://wendywilgus.github.io/Work-Day-Scheduler/",
+    },
+    {
+        title: "Muttel 6",
+        image: muttel6,
+        description: "An interactive web application allows users to create and update their profile information and create reservations for boarding, daycare, and grooming services",
+        code: "https://github.com/jWeibelbootcamp/Google-OAuth-Login",
+        demo: "https://howl-ton.herokuapp.com/",
+    },
+];
+
+export default projectData;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import ProjectData from '../components/ProjectData';
 import './Projects.scss';
 
 const Projects = () => {
     return (
-        <div>
-            <h1>Lukas Vincent Weibel</h1>
+        <div className='project-list'>
+            <div className='row'>
+                {ProjectData.map(Projects => {
+                    return (
+                        <div className='col-md-4'>
+                            <div className='project position-relative '>
+                                <img className="project-image " src={Projects.image} style={{ background: "cover" }} />
+                                <div className='project-content'>
+                                    <h5>{Projects.title}</h5>
+                                    <p>{Projects.description}</p>
+                                    <button className='primary-button'><a href={Projects.code}>Code</a></button>
+                                    <button className='primary-button'><a href={Projects.demo}>Demo</a></button>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })};
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Projects;

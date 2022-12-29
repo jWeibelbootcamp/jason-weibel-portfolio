7a948adbc0bf15929c961f5ef5c97aa442c604db
devWeibel <weibel.jason@gmail.com>
2022-12-29 14:26:43 -0500

    added rendering delay to tech carousel and adjusted background color

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,22 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Image } from "react-bootstrap";
 import Photo from "../images/photo.png";
 import TypewriterBlurb from "../components/Typewriter";
 import TechCarousel from "../components/TechCarousel";
 import './About.scss';
+
+// delays rendering of whatever it wraps
+const useDelayedRender = delay => {
+    const [delayed, setDelayed] = useState(true);
+    useEffect(() => {
+        const timeout = setTimeout(() => setDelayed(false), delay);
+        return () => clearTimeout(timeout);
+    }, [delay]);
+    return fn => !delayed && fn();
+};
+
+const DelayedRender = ({ delay, children }) => useDelayedRender(delay)(() => children);
+// end delay
 
 const About = () => {
     return (
@@ -16,7 +29,9 @@ const About = () => {
                     <TypewriterBlurb />
                 </div>
                 <div className="carouselContainer">
-                    <TechCarousel className='techCarousel' />
+                    <DelayedRender delay={16500}>
+                        <TechCarousel className='techCarousel' />
+                    </DelayedRender>
                 </div>
             </div>
         </div>

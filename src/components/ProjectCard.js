b9f61829fca214df0003cbfd691beca069375049
devWeibel <weibel.jason@gmail.com>
2022-12-31 15:03:43 -0500

    attempting to finish cards... cannot get footer to align to the bottom

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,17 +5,17 @@ const ProjectCard = (props) => {
   return (
     <Card>
       <div className='card-image-container'>
-        <Card.Img variant="top" src={props.image} />
+        <Card.Img variant='top' src={props.image} />
       </div>
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        {/* <Card.Title>{props.title}</Card.Title> */}
         <Card.Text>{props.description}</Card.Text>
-        <div className="card-link-container">
-          <Card.Link href='{props.gitHub}'>GitHub Repo</Card.Link>
-          <text className="link-separator">|</text>
-          <Card.Link href='{props.deployed}'>Deployed App</Card.Link>
-        </div>
       </Card.Body>
+      <Card.Footer>
+        <Card.Link href='{props.gitHub}'>GitHub Repo</Card.Link>
+        <text className='link-separator'>|</text>
+        <Card.Link href='{props.deployed}'>Deployed App</Card.Link>
+      </Card.Footer>
     </Card>
   );
 };
